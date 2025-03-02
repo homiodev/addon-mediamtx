3f5c5d6f61e0c3f0de1d15e0be9fe64e621a3401
Ruslan Masiuk <ruslan.masiuk@tsimagine.com>
2025-03-02 15:31:33 +0200

    1.0.0

---
+++ src/main/java/org/homio/addon/mediamtx/MediaMTXConfig.java
@@ -0,0 +1,12 @@
+package org.homio.addon.mediamtx;
+
+import lombok.extern.log4j.Log4j2;
+import org.homio.api.AddonConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@Configuration
+@AddonConfiguration
+public class MediaMTXConfig {
+
+}
